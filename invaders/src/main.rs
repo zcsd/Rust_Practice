9a57b391d3de3d0cf4d34d4c2a4b22edbdbd3113
zcsd <zc.sd@outlook.com>
2022-10-19 20:57:03 +0800

    prepare ter init

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -4,10 +4,14 @@ use crossterm::{
     ExecutableCommand, 
     event::{self, Event, KeyCode},
 };
+use invaders::{frame::{self, new_frame}, render::render};
 use rusty_audio::Audio;
 use std::{
     error::Error,
-    {io}, time::Duration,
+    io, 
+    time::Duration, 
+    sync::mpsc, 
+    thread,
 };
 
 fn main() -> Result <(), Box<dyn Error>> {
@@ -26,8 +30,28 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render Loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+
+    });
+
     // Game loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -40,9 +64,15 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Draw and render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
