8d0e1cef1b92ba206b9e8bbafffb0eed08081595
zcsd <zc.sd@outlook.com>
2022-10-20 19:14:48 +0800

    render loop

--- invaders/src/render.rs
+++ invaders/src/render.rs
@@ -5,7 +5,6 @@ use crossterm::{
     terminal::{Clear, ClearType}, 
     cursor::MoveTo
 };
-
 use crate::frame::Frame;
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force: bool){
